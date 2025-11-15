9611388183c4b202620c2a49b7f9f7f97c1f928e
SXXHU <dofqls0127@gmail.com>
2025-11-15 21:54:29 +0900

    feat/#31: AnimalRepository desertionNo 조회 메서드 추가

--- src/main/java/com/ganzi/backend/animal/domain/repository/AnimalRepository.java
+++ src/main/java/com/ganzi/backend/animal/domain/repository/AnimalRepository.java
@@ -3,5 +3,8 @@ package com.ganzi.backend.animal.domain.repository;
 import com.ganzi.backend.animal.domain.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AnimalRepository extends JpaRepository<Animal, String>, AnimalRepositoryCustom {
+    Optional<Animal> findByDesertionNo(String desertionNo);
 }
